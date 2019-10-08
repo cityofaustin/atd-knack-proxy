d7f291ba75cf73d47ad8164df10cccc1c8268c51
John Clary <john.clary@austintexas.gov>
2019-10-08 14:30:05 -0500

    drop gunicorn logger

--- __init__.py
+++ __init__.py
@@ -90,9 +90,3 @@ if __name__ == '__main__':
     handler = RotatingFileHandler('log/app.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
-
-
-if __name__ != '__main__':
-    gunicorn_logger = logging.getLogger(‘gunicorn.error’)
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
